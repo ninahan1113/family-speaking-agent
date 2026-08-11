ce985083b1e66a5b4710c483423b2b6bad9a5ec4
raw34 <raw0034@gmail.com>
2026-08-11 15:06:14 +0800

    Refresh mobile asset cache

--- mvp/service-worker.js
+++ mvp/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = "verve-mobile-pilot-v1";
+const CACHE = "verve-mobile-pilot-v2";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
